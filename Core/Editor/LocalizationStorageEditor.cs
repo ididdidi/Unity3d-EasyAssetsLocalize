c7e1ac5e3cbb11f7b1db2c5436ebb1ae600d394c
ididdidi <ididdidi.dev@gmail.com>
2023-05-22 22:05:42 +0300

    Changed the appearance of the LocalizationStorage in the inspector window.

--- Core/Editor/LocalizationStorageEditor.cs
+++ Core/Editor/LocalizationStorageEditor.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 namespace ResourceLocalization
 {
+	/// <summary>
+	/// Responsible for presenting the Localization Storage in the inspector.
+	/// </summary>
 	[CustomEditor(typeof(LocalizationStorage))]
 	public class LocalizationStorageEditor : Editor
 	{
@@ -13,11 +16,14 @@ namespace ResourceLocalization
 		private Localization[] localizations;
 		private bool[] foldouts;
 
-		public void OnEnable()
-		{
-			storage = this.target as LocalizationStorage;
-		}
+		/// <summary>
+		/// Storage link caching.
+		/// </summary>
+		public void OnEnable() => storage = this.target as LocalizationStorage;
 
+		/// <summary>
+		/// Drawing LocalizationStorage in the inspector window.
+		/// </summary>
 		public override void OnInspectorGUI()
 		{
 			DrawDefaultInspector();
@@ -26,30 +32,9 @@ namespace ResourceLocalization
 			DrowButton();
 		}
 
-		private void DrowButton()
-		{
-			GUILayout.BeginHorizontal();
-			GUILayout.FlexibleSpace();
-			if (GUILayout.Button("Open on window", GUILayout.Width(240f), GUILayout.Height(24f)))
-			{
-				LocalizationStorageWindow window = (LocalizationStorageWindow)EditorWindow.GetWindow(typeof(LocalizationStorageWindow), false, storage.name);
-				window.localizationStorage = storage;
-			}
-			GUILayout.FlexibleSpace();
-			GUILayout.EndHorizontal();
-		}
-
-		private void DrowLocalizations()
-		{
-			for(int i=0; i < localizations.Length; i++)
-			{
-				if (foldouts[i] = EditorGUILayout.Foldout(foldouts[i], localizations[i].Name))
-				{
-					LocalizationResourceView.DisplayResources(localizations[i], languages);
-				}
-			}
-		}
-
+		/// <summary>
+		/// Updating LocalizationStorage data in the inspector window.
+		/// </summary>
 		private void UpdateLocalizations()
 		{
 			if (localizations == null || storageVersion != storage.Version)
@@ -58,6 +43,39 @@ namespace ResourceLocalization
 				localizations = storage.Localizations;
 				foldouts = new bool[localizations.Length];
 				storageVersion = storage.Version;
+			}
+		}
+
+		/// <summary>
+		/// Button to display storage resources in a separate window.
+		/// </summary>
+		private void DrowButton()
+		{
+			GUILayout.BeginHorizontal();
+			GUILayout.FlexibleSpace();
+			if (GUILayout.Button("Edit in window", GUILayout.Width(240f), GUILayout.Height(24f)))
+			{
+				LocalizationStorageWindow window = (LocalizationStorageWindow)EditorWindow.GetWindow(typeof(LocalizationStorageWindow), false, storage.name);
+				window.localizationStorage = storage;
+			}
+			GUILayout.FlexibleSpace();
+			GUILayout.EndHorizontal();
+		}
+
+		/// <summary>
+		/// Simple display of localization resources.
+		/// </summary>
+		private void DrowLocalizations()
+		{
+			var style = new GUIStyle(EditorStyles.boldLabel) { alignment = TextAnchor.MiddleCenter };
+			EditorGUILayout.LabelField("Localization resources", style, GUILayout.ExpandWidth(true));
+
+			for(int i=0; i < localizations.Length; i++)
+			{
+				if (foldouts[i] = EditorGUILayout.Foldout(foldouts[i], localizations[i].Name))
+				{
+					LocalizationResourceView.DisplayResources(localizations[i], languages);
+				}
 			}
 		}
 	}
