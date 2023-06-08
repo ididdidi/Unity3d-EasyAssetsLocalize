61e695d2e0d3488bf920bc7ec81f3c9127a5d6bb
ididdidi <ididdidi.dev@gmail.com>
2023-06-08 12:27:31 +0300

    UPD: Code comments

--- Core/IResource.cs
+++ Core/IResource.cs
@@ -1,8 +1,19 @@
 ﻿namespace ResourceLocalization
 {
+    /// <summary>
+    /// Interface for interacting with resources.
+    /// </summary>
     public interface IResource
     {
+        /// <summary>
+        /// Property to get and set data
+        /// </summary>
         object Data { get; set; }
+
+        /// <summary>
+        /// Method for making a copy of an object.
+        /// </summary>
+        /// <returns>Object with similar data</returns>
         IResource Clone();
     }
 }
