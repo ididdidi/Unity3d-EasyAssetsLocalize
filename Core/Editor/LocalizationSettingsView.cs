bcec83a44545575307083e15a922a2ef5e1a0f19
ididdidi <ididdidi.dev@gmail.com>
2023-07-11 18:20:57 +0300

    ADD: Code Settings view

---
+++ Core/Editor/LocalizationSettingsView.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace ResourceLocalization
+{
+    public class LocalizationSettingsView
+    {
+        private readonly Color background = new Color(0.22f, 0.22f, 0.22f);
+        private LocalizationStorageWindow window;
+
+        public LocalizationSettingsView(LocalizationStorageWindow window)
+        {
+            this.window = window ?? throw new System.ArgumentNullException(nameof(window));
+        }
+
+        public void OnGUI(Rect position)
+        {
+            EditorGUI.DrawRect(position, background);
+            GUI.Label(position, GUIContent.none, "grey_border");
+
+            GUILayout.BeginArea(position);
+            GUILayout.Space(2);
+            GUILayout.BeginHorizontal(EditorStyles.inspectorDefaultMargins);
+            GUILayout.FlexibleSpace();
+            GUILayout.Label("Settings", "AM MixerHeader");
+            GUILayout.FlexibleSpace();
+            if (GUILayout.Button("Back")) { window.ShowSettings = false; }
+            GUILayout.EndHorizontal();
+            GUILayout.EndArea();
+        }
+    }
+}
--- Core/Editor/LocalizationStorageWindow.cs
+++ Core/Editor/LocalizationStorageWindow.cs
@@ -12,21 +12,23 @@ namespace ResourceLocalization
 		public const float MIN_WIDTH = 720f;
 		public const float MIN_HIGHT = 320f;
 
-		public Color background = new Color(0.22f, 0.22f, 0.22f);
+		private Color background = new Color(0.22f, 0.22f, 0.22f);
 
 		// Data renderer in a editor window
 		private SearchTreeView searchView;
 		private LocalizationView localizationView;
+		private LocalizationSettingsView settingsView;
 		private int storageVersion;
 		
 		private static LocalizationStorage LocalizationStorage { get => LocalizationManager.LocalizationStorage; }
+		public bool ShowSettings { get; set; }
 
 		private void OnEnable()
 		{
 			localizationView = new LocalizationView(this, LocalizationStorage);
-			var provider = new LocalizationSearchProvider(LocalizationStorage, (tag) => { localizationView.Tag = tag; return false; }, (tag) => localizationView.Tag = tag);
+			var provider = new LocalizationSearchProvider(LocalizationStorage, (tag) => { ShowSettings = false; localizationView.Tag = tag; return false; }, (tag) => localizationView.Tag = tag);
 			searchView = new SearchTreeView(this, provider);
-
+			settingsView = new LocalizationSettingsView(this);
 			//hideFlags = HideFlags.HideAndDontSave;
 			//wantsMouseMove = true;
 		}
@@ -57,9 +59,26 @@ namespace ResourceLocalization
 			var rect = new Rect(0, 0, 320, position.height);
 			searchView?.OnGUI(rect);
 
+			if (string.IsNullOrEmpty(searchView.SearchKeyword) && SettingsButton(new Rect(302, 8, 20, 20)))
+			{
+				ShowSettings = true;
+			}
+
 			rect.x = 319;
 			rect.width = position.width - 320;
-			localizationView?.OnGUI(rect);
+			if (!ShowSettings)
+			{
+				localizationView?.OnGUI(rect);
+			}
+			else
+			{
+				settingsView?.OnGUI(rect);
+			}
+		}
+
+		private bool SettingsButton(Rect rect)
+		{
+			return GUI.Button(rect, EditorGUIUtility.IconContent("_Popup"), GUIStyle.none);
 		}
 	}
 }
