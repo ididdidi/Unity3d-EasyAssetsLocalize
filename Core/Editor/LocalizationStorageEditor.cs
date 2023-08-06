984951f97f0a75e88d6986aab81aa54d20c5ce45
ididdidi <ididdidi.dev@gmail.com>
2023-08-06 21:07:07 +0300

    Adapted SearchTreeView for Editor

--- Core/Editor/LocalizationStorageEditor.cs
+++ Core/Editor/LocalizationStorageEditor.cs
@@ -8,15 +8,10 @@ namespace SimpleLocalization
 	/// Responsible for presenting the Localization Storage in the inspector.
 	/// </summary>
 	[CustomEditor(typeof(LocalizationStorage))]
-	public partial class LocalizationStorageEditor : Editor
+	public partial class LocalizationStorageEditor : Editor, IDisplay
 	{
 		private LocalizationStorage storage;
-		private int storageVersion;
-
-		private bool foldout;
-		private Localization[] localizationTags;
-		private bool[] foldoutItems;
-		private TestSearchEditorView testView;
+		private SearchEditorView testView;
 
 		/// <summary>
 		/// Storage link caching.
@@ -24,7 +19,7 @@ namespace SimpleLocalization
 		public void OnEnable()
 		{
 			storage = this.target as LocalizationStorage;
-			testView = new TestSearchEditorView(this, new LocalizationSearchProvider(storage, (i) => { Debug.Log(i); return false; }));
+			testView = new SearchEditorView(this, new LocalizationSearchProvider(storage, (i) => { Debug.Log(i); return false; }));
 		}
 
 		/// <summary>
@@ -33,57 +28,6 @@ namespace SimpleLocalization
 		public override void OnInspectorGUI()
 		{
 			testView.OnGUI();
-		//	DrawDefaultInspector();
-		//	UpdateLocalizations();
-		//	DrowLocalizations();
-		//	DrawButton();
-		}
-
-		/// <summary>
-		/// Updating LocalizationStorage data in the inspector window.
-		/// </summary>
-		private void UpdateLocalizations()
-		{
-			if (localizationTags == null || storageVersion != storage.Version)
-			{
-				localizationTags = storage.Localizations;
-				foldoutItems = new bool[localizationTags.Length];
-				storageVersion = storage.Version;
-			}
-		}
-
-		/// <summary>
-		/// Button to display storage resources in a separate window.
-		/// </summary>
-		private void DrawButton()
-		{
-			if (ExtendedEditor.CenterButton("Edit in window"))
-			{
-				LocalizationStorageWindow.Show();
-			}
-		}
-
-		/// <summary>
-		/// Simple display of localization resources.
-		/// </summary>
-		private void DrowLocalizations()
-		{
-			EditorGUI.indentLevel++;
-			if (foldout = EditorGUILayout.Foldout(foldout, "Localization resources"))
-			{
-				EditorGUI.indentLevel++;
-				for (int i = 0; i < localizationTags.Length; i++)
-				{
-					if (foldoutItems[i] = EditorGUILayout.Foldout(foldoutItems[i], localizationTags[i].Name))
-					{
-						EditorGUI.BeginDisabledGroup(localizationTags[i].IsDefault);
-						LocalizationView.DrawResources(localizationTags[i], LocalizationManager.Languages, GUILayout.Height(50f));
-						EditorGUI.EndDisabledGroup();
-					}
-				}
-				EditorGUI.indentLevel--;
-			}
-			EditorGUI.indentLevel--;
 		}
 	}
 }
