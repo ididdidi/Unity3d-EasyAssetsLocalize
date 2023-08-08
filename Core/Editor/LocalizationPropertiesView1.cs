b6e20e2f318f45a24ec8a2287f9f893bed4231c5
ididdidi <ididdidi.dev@gmail.com>
2023-08-08 16:26:28 +0300

    UPD: ExtendedEditor

---
+++ Core/Editor/LocalizationPropertiesView1.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityExtended;
+
+namespace SimpleLocalization
+{
+    public class LocalizationPropertiesView1 : IEditorView
+    {
+        private readonly Color LightSkin = new Color(0.77f, 0.77f, 0.77f);
+        private readonly Color DarkSkin = new Color(0.22f, 0.22f, 0.22f);
+
+        private Color Background => EditorGUIUtility.isProSkin ? DarkSkin : LightSkin;
+
+        public void OnGUI(Rect position)
+        {
+            EditorGUI.DrawRect(position, Background);
+        }
+    }
+}
--- Core/Editor/LocalizationStorageEditor.cs
+++ Core/Editor/LocalizationStorageEditor.cs
@@ -11,7 +11,9 @@ namespace SimpleLocalization
 	public partial class LocalizationStorageEditor : Editor, IDisplay
 	{
 		private LocalizationStorage storage;
-		private SearchEditorView testView;
+		private SearchEditorView searchView;
+		private LocalizationPropertiesView1 propertiesView;
+		private IEditorView currentView;
 
 		/// <summary>
 		/// Storage link caching.
@@ -19,7 +21,10 @@ namespace SimpleLocalization
 		public void OnEnable()
 		{
 			storage = this.target as LocalizationStorage;
-			testView = new SearchEditorView(this, new LocalizationSearchProvider(storage, (i) => { Debug.Log(i); return false; }));
+			searchView = new SearchEditorView(this, new LocalizationSearchProvider(storage, (i) => { Debug.Log(i); return false; }), false);
+			searchView.OptionButton = ShowPropertiesButton;
+			propertiesView = new LocalizationPropertiesView1();
+			currentView = searchView;
 		}
 
 		/// <summary>
@@ -27,7 +32,18 @@ namespace SimpleLocalization
 		/// </summary>
 		public override void OnInspectorGUI()
 		{
-			testView.OnGUI();
+			var rect = GUILayoutUtility.GetRect(EditorGUIUtility.currentViewWidth, searchView.HeightInGUI);
+			rect.width = EditorGUIUtility.currentViewWidth;
+			currentView.OnGUI(rect);
+		}
+
+		/// <summary>
+		/// Button to show properties
+		/// </summary>
+		/// <param name="rect">Position</param>
+		private void ShowPropertiesButton(Rect rect)
+		{
+			if (GUI.Button(rect, EditorGUIUtility.IconContent("_Popup"), GUIStyle.none)) { currentView = propertiesView; }
 		}
 	}
 }
