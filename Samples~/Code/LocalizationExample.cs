93be28a5e487f8341f9e50ea7cf626d921d43f23
ididdidi <ididdidi.dev@gmail.com>
2023-08-30 14:48:51 +0300

    ADD: Samples

---
+++ Samples~/Code/LocalizationExample.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using EasyAssetsLocalize;
+
+public class LocalizationExample : MonoBehaviour
+{
+    [SerializeField] Text language;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetLanguge(LocalizationManager.Language);
+    }
+
+    public void PrevLanguage()
+    {
+        LocalizationManager.SetPrevLanguage();
+        SetLanguge(LocalizationManager.Language);
+    }
+
+    public void NextLanguage()
+    { 
+        LocalizationManager.SetNextLanguage();
+        SetLanguge(LocalizationManager.Language);
+    }
+
+    private void SetLanguge(Language language)
+    {
+        this.language.text = language.ToString();
+    }
+}
